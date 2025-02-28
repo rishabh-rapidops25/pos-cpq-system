b07ea2b7e2a13cca108df07a3692168cbf6e5979
Rishabh <rishabh.prajapati@rapidops.com>
2025-02-28 09:47:21 +0530

    Updated search category logic

--- product-service/src/routes/categoryRoutes.ts
+++ product-service/src/routes/categoryRoutes.ts
@@ -20,7 +20,7 @@ const router = express.Router();
 
 router.post("/create-category", validate(createCategorySchema), createCategory);
 router.post("/", validate(getAllCategoriesSchema), getAllCategories);
-router.get("/search/:search", validate(searchCategorySchema), searchCategories);
+router.get("/:search", validate(searchCategorySchema), searchCategories);
 router.get("/:id", getCategoryById);
 router.post(
   "/update-category/:id",
