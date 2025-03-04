36ceafe98ba847bd56cd435551ebed6998c36feb
Rishabh <rishabh.prajapati@rapidops.com>
2025-03-04 11:50:10 +0530

    Remove folder from repository

--- user-service/dist/repository/user.repository.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userRepository = void 0;
-const ormconfig_1 = require("../config/ormconfig");
-const User_1 = require("../models/User");
-exports.userRepository = ormconfig_1.AppDataSource.getRepository(User_1.User);
