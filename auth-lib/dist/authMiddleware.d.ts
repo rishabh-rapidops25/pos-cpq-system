8d8e7e038baccd3420389a2d765c5553dc5f4602
Rishabh <rishabh.prajapati@rapidops.com>
2025-03-04 11:53:54 +0530

    Remove folder from repository

--- auth-lib/dist/authMiddleware.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-import { JwtPayload } from "jsonwebtoken";
-interface AuthRequest extends Request {
-    user?: string | JwtPayload;
-}
-export declare const authMiddleware: (req: AuthRequest, res: Response, next: NextFunction) => void;
-export {};
