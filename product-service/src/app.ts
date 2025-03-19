e9c1ac0f147d365a3a60cd1e1577428aaa02a628
Rishabh <rishabh.prajapati@rapidops.com>
2025-03-19 13:48:54 +0530

    Remove unused code

--- product-service/src/app.ts
+++ product-service/src/app.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import { PORT, HOST, NODE_ENV } from './constants';
