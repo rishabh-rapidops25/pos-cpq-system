930462c42bb93586e381fc40ab3eb541ec2f6716
Rishabh <rishabh.prajapati@rapidops.com>
2025-03-24 12:30:18 +0530

    Added gateway config

--- gateway/src/app.ts
+++ gateway/src/app.ts
@@ -1,21 +1,43 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import { createProxyMiddleware } from "http-proxy-middleware";
+import express from 'express';
+import cors from 'cors';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import { logger } from 'shared-constants';
+import { PORT, PRODUCT_SERVICE, USER_SERVICE } from './constant';
 
-dotenv.config();
 const app = express();
 
-app.use(cors());    
+app.use(cors());
 app.use(express.json());
 
 // Proxy to User Service
-app.use("/api/users", createProxyMiddleware({ target: "http://user-service:3001", changeOrigin: true }));
+// This proxy is used to forward requests from the API Gateway to the User Service.
+// The `changeOrigin` option ensures that the origin of the host header in the proxied request
+// matches the target URL, which is necessary for some backend services to accept the request.
+app.use(
+  '/api/users',
+  createProxyMiddleware({
+    target: USER_SERVICE,
+    changeOrigin: true,
+  })
+);
+
+// Proxy to Product Service: Routes requests from '/api/products' to the Product Service.
+// This proxy forwards requests to the target URL specified in the PRODUCT_SERVICE_URL environment variable
+// or defaults to 'http://product-service:3002' if the variable is not set. It also ensures the origin is changed.
 
 // Proxy to Product Service
-app.use("/api/products", createProxyMiddleware({ target: "http://product-service:3002", changeOrigin: true }));
+app.use(
+  '/api/products',
+  createProxyMiddleware({
+    target: PRODUCT_SERVICE,
+    changeOrigin: true,
+  })
+);
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`API Gateway running on port ${PORT}`);
+  try {
+    logger.info(`API Gateway running on port ${PORT}`);
+  } catch (error) {
+    logger.error(`Error starting API Gateway: ${error}`);
+  }
 });
---
+++ gateway/src/constant/index.ts
@@ -0,0 +1,4 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const { PORT, USER_SERVICE, PRODUCT_SERVICE } = process.env;
